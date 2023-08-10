457a98cfcc302222e357e064cc0cabcf260e19bb
kintokeanu <levu@kali>
2023-08-10 22:36:36 +0300

    testing basemode

--- models/base_model.py
+++ models/base_model.py
@@ -64,6 +64,7 @@ class BaseModel:
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
 
+
 # data = {
 #     "id": "123",
 #     "created_at": "2023-07-19T15:30:00.000000",
